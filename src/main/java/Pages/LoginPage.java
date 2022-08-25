97c2681da211f596173d0d411ac5c06fd83c09db
Sadafbakhtavar <sadafbakhtavar@gmail.com>
2022-08-25 13:30:44 -0400

    Added one test

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,21 @@
+package Pages;
+
+import browser.Browser;
+import browser.Info;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    @FindBy(css = "input[name='username']") private WebElement username;
+    @FindBy(css = "input[name='password']") private WebElement password;
+    @FindBy(className = "orangehrm-login-button") private WebElement loginButton;
+
+    public void loginToPIMPage(){
+        username.sendKeys(Info.USER_NAME);
+        password.sendKeys(Info.PASSWORD);
+        loginButton.click();
+        return PageFactory.initElements(Browser.driver, PIMPage.class);
+    }
+}
